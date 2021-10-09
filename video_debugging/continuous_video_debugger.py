0d0dba5368752f6dd4d5ca199b8d49437da67159
marcinwasowicz <marcinkwasowicz@gmail.com>
2021-10-09 18:21:55 +0200

    Implement script for live/file video analysis frame-by-frame.

---
+++ video_debugging/continuous_video_debugger.py
@@ -0,0 +1,26 @@
+import logging
+import sys
+import time
+
+from gesture_recognition.gesture_recognizer import GestureRecognizer
+from video_debugging import FrameSource
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("video file debugger")
+
+if __name__ == '__main__':
+    video_path = sys.argv[1] if sys.argv[1] != 'live' else 0
+    pretrained_recognizer = GestureRecognizer.from_pickle_binary(sys.argv[2])
+
+    source = FrameSource(video_path)
+
+    for counter, fps, frame in source:
+        start_inference = time.time()
+        classification = pretrained_recognizer.recognize(frame)
+        inference_time = time.time() - start_inference
+
+        seconds_passed = counter / fps
+        video_time_stamp = time.strftime('%H:%M:%S', time.gmtime(seconds_passed))
+
+        if classification is not None:
+            logger.info(f"Recognized {classification[0]} at time {video_time_stamp}. Inference took {inference_time}.")
