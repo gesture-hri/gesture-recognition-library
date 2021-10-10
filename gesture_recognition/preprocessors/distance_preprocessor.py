2d4a975c83c4a8952590dfa7dc8fd0e63f101738
marcinwasowicz <marcinkwasowicz@gmail.com>
2021-10-10 11:54:29 +0200

    Allow to choose between L1 and L2 metrics in DistancePreprocessor.

--- gesture_recognition/preprocessors/distance_preprocessor.py
+++ gesture_recognition/preprocessors/distance_preprocessor.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
@@ -9,6 +11,21 @@ class DistancePreprocessor(Preprocessor):
     Distance preprocessor extracts 3D coordinates from joint landmarks, calculates norm of euclidean distance
     between each pair of landmarks and flattens the output.
     """
+    class Metrics(Enum):
+        """
+        Class to hold available metrics as enumeration type.
+        """
+        L1 = 1
+        L2 = 2
+
+    def __init__(self, metrics):
+        """
+        :param metrics: Metrics used to calculate vector norm in preprocess method
+        """
+        if not isinstance(metrics, DistancePreprocessor.Metrics):
+            raise ValueError("Currently only metrics defined in DistancePreprocessor class are supported")
+
+        self.metrics = metrics
 
     def preprocess(self, mediapipe_output, *args, **kwargs):
         landmarks = np.array(
@@ -22,7 +39,7 @@ class DistancePreprocessor(Preprocessor):
         return (
             StandardScaler()
             .fit_transform(
-                np.linalg.norm((landmarks[:, np.newaxis] - landmarks), axis=1),
+                np.linalg.norm((landmarks[:, np.newaxis] - landmarks), axis=2, ord=self.metrics.value),
             )
             .reshape((-1,))
         )
